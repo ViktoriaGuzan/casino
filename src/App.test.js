9c6c07ff7ed2e60d88e40c3f8972ca2d79200b6e
Viktoria Huzandrova <viktoriahuzandrova@MacBook-Air-Viktoria.local>
2022-09-01 19:03:22 +0200

    Add unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('App title valid', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appTitle = document.querySelector('header > h2');
+  expect(appTitle).toHaveTextContent('casino');
 });
